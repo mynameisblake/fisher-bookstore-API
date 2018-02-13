81c64a17cef415f9c6d5dbbb3207d2f7e48f63c1
Blake Crowley <crowley.124@osu.edu>
2018-02-12 20:26:17 -0500

    Added Books and Authors API

---
+++ Models/Author.cs
@@ -0,0 +1,14 @@
+using System; 
+
+namespace Fisher.Bookstore.Api.Models
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }   
+        public DateTime BirthDate { get; set; }  
+          
+         
+           
+    }
+}
